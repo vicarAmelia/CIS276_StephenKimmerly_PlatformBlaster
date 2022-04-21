2ae4d0a9733f2833886cb0273840b99dc0ef8bd9
vicarAmelia <98558073+vicarAmelia@users.noreply.github.com>
2022-04-21 15:41:44 -0400

    Edited controller script

--- Platform Blaster/Assets/Scripts/PlayerInput.cs
+++ Platform Blaster/Assets/Scripts/PlayerInput.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent (typeof (Controller2D))]
 public class PlayerInput : MonoBehaviour
 {
+    Controller2D controller;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
